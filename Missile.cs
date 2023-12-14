cfff76c18abf52597dbda76b766e81598d31cb6c
Milioner999 <150339960+Milioner999@users.noreply.github.com>
2023-12-14 13:58:05 +0400

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 2f;
+    //Update is called once per frame
+
+    void Update()
+    {
+        Destroy(gameObject, 3);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        Destroy(other.gameObject);
+    }
+}
