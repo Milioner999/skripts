8d2c3e8755080bac2ab7ec238eb5e834a00f1116
Milioner999 <150339960+Milioner999@users.noreply.github.com>
2023-12-21 14:20:40 +0300

    Add files via upload

--- Timer.cs
+++ Timer.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public int minutes;
     public float seconds;
+    public TMPro.TextMeshProUGUI timer;
 
     //Цикл обновления 0.01
     void Update()
     {
+        timer.text = timer.text + minutes + ":" + seconds;
         seconds -= Time.deltaTime;
 
         if (seconds <= 0)
